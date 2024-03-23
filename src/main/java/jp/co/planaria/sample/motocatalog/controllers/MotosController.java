1be02e316c573837f6fc810fb9c27040a969b874
shoot1234 <phenix.endli9@gmail.com>
2024-03-23 11:41:03 +0900

    排他エラー発生時ドロップダウンが表示されるようにした

--- src/main/java/jp/co/planaria/sample/motocatalog/controllers/MotosController.java
+++ src/main/java/jp/co/planaria/sample/motocatalog/controllers/MotosController.java
@@ -106,7 +106,7 @@ public class MotosController {
   }
 
   @PostMapping("/motos/save")
-  public String save(@ModelAttribute MotoForm motoForm, BindingResult result) {
+  public String save(@ModelAttribute MotoForm motoForm, BindingResult result, Model model) {
     try {
       log.info("motoForm:{}", motoForm);
       Motorcycle moto = new Motorcycle();
@@ -120,6 +120,8 @@ public class MotosController {
       return "redirect:/motos";
 
     } catch (OptimisticLockingFailureException e) {
+      // ブランドリストを準備
+      this.setBrands(model);
       result.addError(new ObjectError("global", e.getMessage()));
       return "moto";
     }
