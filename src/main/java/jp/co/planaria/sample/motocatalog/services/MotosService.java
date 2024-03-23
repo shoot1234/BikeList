429c2090703c52efff13839dfd256047a7a6c8da
shoot1234 <phenix.endli9@gmail.com>
2024-03-23 11:31:26 +0900

    エラーメッセージを整理した

--- src/main/java/jp/co/planaria/sample/motocatalog/services/MotosService.java
+++ src/main/java/jp/co/planaria/sample/motocatalog/services/MotosService.java
@@ -1,10 +1,13 @@
 package jp.co.planaria.sample.motocatalog.services;
 
 import java.util.List;
+import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import jp.co.planaria.sample.motocatalog.beans.Brand;
 import jp.co.planaria.sample.motocatalog.beans.Motorcycle;
@@ -16,7 +19,11 @@ import jp.co.planaria.sample.motocatalog.mappers.MotorcycleMapper;
 public class MotosService {
 
   @Autowired
+  MessageSource messageSource;
+
+  @Autowired
   MotorcycleMapper motorcycleMapper;
+
   @Autowired
   BrandMapper brandMapper;
 
@@ -38,15 +45,18 @@ public class MotosService {
    * @return 更新件数
    */
 
+   @Transactional
   public int save(Motorcycle moto) {
     int cnt = motorcycleMapper.update(moto);
     // 更新できなかった場合、更新されたか削除されたため楽観的排他エラーとする
     if (cnt == 0) {
-      throw new OptimisticLockingFailureException("楽観的排他エラー");
+      throw new OptimisticLockingFailureException(messageSource.getMessage("error.OptimisticLockingFailure",
+       null, Locale.JAPANESE));
     }
     // 2件以上更新は想定外(SQLの不備の可能性)
     if (cnt > 1) {
-      throw new RuntimeException("2件以上更新されました。");
+      throw new RuntimeException(messageSource.getMessage("error.Runtime",
+       new String[]{"2件以上更新されました。"}, Locale.JAPANESE));
     }
     return cnt;
   }  
