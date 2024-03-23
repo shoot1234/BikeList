85449a04f0a8cb86ddb987c432cc45aa9e2d6888
shoot1234 <phenix.endli9@gmail.com>
2024-03-23 09:59:31 +0900

    楽観的排他制御した

--- src/main/java/jp/co/planaria/sample/motocatalog/controllers/MotosController.java
+++ src/main/java/jp/co/planaria/sample/motocatalog/controllers/MotosController.java
@@ -12,6 +12,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -21,8 +22,6 @@ import jp.co.planaria.sample.motocatalog.beans.SearchForm;
 import jp.co.planaria.sample.motocatalog.forms.MotoForm;
 import jp.co.planaria.sample.motocatalog.services.MotosService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @Slf4j// ログ部品を使えるようになる
